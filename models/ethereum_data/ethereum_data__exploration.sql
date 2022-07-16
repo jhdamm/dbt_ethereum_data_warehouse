39131cf0f2dc07887ac3f34609c4290bd0c4fc4b
Jan Damm <service+git@jandamm.me>
2022-07-15 17:09:28 -0700

    Creating a first view

--- models/ethereum_data/ethereum_data__exploration.sql
+++ models/ethereum_data/ethereum_data__exploration.sql
@@ -1,1 +1,9 @@
-with source
+with source_tokens as (
+    select * from {{ source('crypto_ethereum_snapshot', 'tokens') }}
+)
+
+final as (
+    select * from source_tokens
+)
+
+select * from final
